ff03fc834d49f423e279af312a12f7deffaf25a2
PartMan <parth.mane@sprinklr.com>
2025-09-25 00:24:03 +0530

    chore: More types!

--- src/decoder.ts
+++ src/decoder.ts
@@ -1,19 +1,20 @@
 import CryptoJS from "crypto-js";
 import { CSHARP_HEADER, AES_KEY_STRING } from "./constants.js";
+import type { SaveData } from "./types.ts";
 
-export function removeHeader(fileBytes) {
+export function removeHeader(fileBytes: Uint8Array) {
   const noHeader = fileBytes.subarray(CSHARP_HEADER.length, fileBytes.length - 1);
 
   let lengthPrefix = 0;
   for (let i = 0; i < 5; i++) {
     lengthPrefix++;
-    if ((noHeader[i] & 0x80) == 0) break;
+    if ((noHeader[i]! & 0x80) == 0) break;
   }
 
   return noHeader.subarray(lengthPrefix);
 }
 
-export function decodeFile(fileBytes) {
+export function decodeFile(fileBytes: Uint8Array): SaveData {
   const noHeader = removeHeader(fileBytes);
   const b64String = new TextDecoder("latin1").decode(noHeader);
   const encryptedWords = CryptoJS.enc.Base64.parse(b64String);
