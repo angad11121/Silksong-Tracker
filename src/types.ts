33848953d7e74cf8c1c4c6a73383c4e8da688cb5
PartMan <parth.mane@sprinklr.com>
2025-09-25 03:09:09 +0530

    chore: Add data files for type assertions

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,22 @@
-export type SaveData = {}
+type PlayerData = {};
+
+type PersistentValue<T> = {
+  SceneName: string;
+  ID: string;
+  Value: T;
+  Mutator: 0 | 1;
+};
+type PersistentBool = PersistentValue<boolean>;
+type PersistentInt = PersistentValue<number>;
+type GeoRock = PersistentValue<number>;
+
+type SceneData = {
+  persistentBools: { serializedList: PersistentBool[] };
+  persistentInts: { serializedList: PersistentInt[] };
+  geoRocks: { serializedList: GeoRock[] };
+};
+
+export type SaveData = {
+  playerData: PlayerData;
+  sceneData: SceneData;
+};
