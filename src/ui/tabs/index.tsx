1e2d20e8758e98dea5296b7bf9ff828bf034174d
PartMan <parth.mane@sprinklr.com>
2025-10-04 03:28:04 +0530

    chore: Fix tab switcher

--- src/ui/tabs/index.tsx
+++ src/ui/tabs/index.tsx
@@ -1,25 +1,52 @@
 import type { SaveData } from '@/types';
 import { TabType } from '@/ui/tabs/constants';
-import { useState, type ReactElement } from 'react';
+import { useState, type ReactElement, type ReactNode } from 'react';
 
 import { RawDataDisplay } from './data';
 import { PercentageDisplay } from './percentage';
 import { MementoDisplay } from './memento';
 
-const TabRenderers = {
-  [TabType.RawData]: RawDataDisplay,
-  [TabType.PercentageData]: PercentageDisplay,
-  [TabType.MementoData]: MementoDisplay,
+const TabRenderers: Record<
+  TabType,
+  {
+    Renderer: ({ data }: { data: SaveData }) => ReactNode;
+    hideTab?: boolean;
+    title: string;
+    id: TabType;
+  }
+> = {
+  [TabType.RawData]: {
+    Renderer: RawDataDisplay,
+    title: 'Raw Data',
+    id: TabType.RawData,
+  },
+  [TabType.PercentageData]: {
+    Renderer: PercentageDisplay,
+    hideTab: true,
+    title: 'Percentage Data',
+    id: TabType.PercentageData,
+  },
+  [TabType.MementoData]: {
+    Renderer: MementoDisplay,
+    hideTab: true,
+    title: 'Memento Data',
+    id: TabType.MementoData,
+  },
 };
 
 export function DataRenderer({ data }: { data: SaveData }): ReactElement {
   const [tab, setTab] = useState<TabType>(TabType.RawData);
-  const Renderer = TabRenderers[tab];
+  const { Renderer } = TabRenderers[tab];
   return (
     <div>
       <div>
-        <button onClick={() => setTab(TabType.RawData)}>Raw Data</button>
-        <button onClick={() => setTab(TabType.PercentageData)}>Percentage Data</button>
+        {Object.values(TabRenderers).map(value =>
+          value.hideTab ? null : (
+            <button key={value.id} onClick={() => setTab(value.id)}>
+              {value.title}
+            </button>
+          ),
+        )}
       </div>
       <Renderer data={data} />
     </div>
