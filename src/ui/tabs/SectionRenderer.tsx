25c428d8907326bbd2f1d9f12c98fbfd5acdf1db
PartMan <parth.mane@sprinklr.com>
2025-10-07 14:02:46 +0530

    feat: Add dropdowns and better displays for percentages

--- src/ui/tabs/SectionRenderer.tsx
+++ src/ui/tabs/SectionRenderer.tsx
@@ -6,20 +6,25 @@ export function SectionRenderer<ExtraCtx = null>({
   depth = 0,
   data,
   sections,
+  parent,
   SectionTitleRenderer = ({ section, children }) => (
-    <>
-      <h3>{section.title}</h3>
-      <h4>{section.subtext}</h4>
+    <details open>
+      <summary>
+        <h3>{section.title}</h3>
+        <h4>{section.subtext}</h4>
+      </summary>
       {children}
-    </>
+    </details>
   ),
 }: {
   depth?: number;
   data: SaveData;
   sections: (Section<ExtraCtx> | LeafSection)[];
+  parent?: Section<ExtraCtx> | null;
   SectionTitleRenderer?: (props: {
     section: Section<ExtraCtx>;
     children: ReactElement;
+    parent: Section<ExtraCtx> | null;
   }) => ReactElement;
 }): ReactElement {
   return (
@@ -33,10 +38,11 @@ export function SectionRenderer<ExtraCtx = null>({
             {'render' in section ? (
               <>{section.render({ saveData: data, depth, entry: section })}</>
             ) : (
-              <SectionTitleRenderer section={section}>
+              <SectionTitleRenderer section={section} parent={parent ?? null}>
                 <SectionRenderer
                   depth={depth + 1}
                   data={data}
+                  parent={section}
                   sections={
                     typeof section.children === 'function'
                       ? section.children(data)
--- src/ui/tabs/percentage/index.tsx
+++ src/ui/tabs/percentage/index.tsx
@@ -10,7 +10,7 @@ export function PercentageDisplay({ data }: { data: SaveData }): ReactElement {
       sections={SectionGenerator}
       depth={0}
       data={data}
-      SectionTitleRenderer={({ section, children }) => {
+      SectionTitleRenderer={({ section, children, parent }) => {
         const getPercentage = section.ctx.getPercentage;
         const currentPercentage =
           typeof getPercentage === 'function'
@@ -19,17 +19,26 @@ export function PercentageDisplay({ data }: { data: SaveData }): ReactElement {
         const maxPercentage = section.ctx.maxPercentage;
 
         return (
-          <div className="p-4 bg-[rgba(0,0,0,0.5)] rounded-xl">
-            <h3>
-              {section.title} (
-              {currentPercentage === maxPercentage
-                ? maxPercentage
-                : `${currentPercentage}/${maxPercentage}`}
-              %)
-            </h3>
-            <h4>{section.subtext}</h4>
+          <details
+            open={currentPercentage < maxPercentage || !parent}
+            className="p-4 bg-[rgba(0,0,0,0.5)] rounded-xl"
+          >
+            <summary>
+              <h3 className="inline mx-2 pt-2">
+                {section.title} (
+                {currentPercentage === maxPercentage ? (
+                  <span className="text-green-500">{maxPercentage}%</span>
+                ) : (
+                  <>
+                    <span className="text-red-500">{currentPercentage}%</span>/{maxPercentage}%
+                  </>
+                )}
+                )
+              </h3>
+              <h4>{section.subtext}</h4>
+            </summary>
             {children}
-          </div>
+          </details>
         );
       }}
     />
