9fb8e18ea9152db6c9cfa50ced752f027d607724
PartMan <parth.mane@sprinklr.com>
2025-10-07 21:36:53 +0530

    chore: Fix preload image layout reflow

--- src/ui/preload.tsx
+++ src/ui/preload.tsx
@@ -5,8 +5,8 @@ import type { ReactElement } from 'react';
 export function Preload(): ReactElement {
   return (
     <div className="absolute top-0 h-0.5 invisible">
-      <img src={LiteMapImage} alt="Game Map" />
-      <img loading="lazy" src={MapImage} alt="Game Map" />
+      <img src={LiteMapImage} alt="Game Map" width={1} height={1} />
+      <img loading="lazy" src={MapImage} alt="Game Map" width={1} height={1} />
     </div>
   );
 }
