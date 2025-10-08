4fe86c54b1949ef6e97069998798280ed65b049f
PartMan <parth.mane@sprinklr.com>
2025-10-09 03:34:52 +0530

    feat: Add Memory Locket data

---
+++ src/info/lockets.ts
@@ -0,0 +1,273 @@
+import { getQuest, getScene, type MetadataKey } from '@/parser/metadata';
+import { Locations } from '@/info/locations';
+import type { SaveData } from '@/parser/types';
+import type { MapLocation } from '@/ui/components/map';
+
+// donotpush REF DATA
+
+const Flintbeetles: { key: MetadataKey; getMarker: (saveData: SaveData) => MapLocation[] }[] = [
+  {
+    key: 'rockRollerDefeated_bone01',
+    getMarker: () => [
+      {
+        label: 'Defeat the Flintbeetle and collect the Flintgem.',
+        location: { x: 1304, y: 2598 },
+      },
+    ],
+  },
+  {
+    key: 'rockRollerDefeated_bone06',
+    getMarker: () => [
+      {
+        label: 'Defeat the Flintbeetle and collect the Flintgem.',
+        location: { x: 1610, y: 2316 },
+      },
+    ],
+  },
+  // TODO: This location is different depending on playerData.bone03_openedTrapdoorForRockRoller
+  {
+    key: 'rockRollerDefeated_bone07',
+    getMarker: () => [
+      {
+        label: 'Defeat the Flintbeetle and collect the Flintgem.',
+        location: { x: 2024, y: 2451 },
+      },
+    ],
+  },
+];
+
+export const MemoryLockets: {
+  id: number;
+  desc: string;
+  has: (saveData: SaveData) => boolean | undefined;
+  markers: ((saveData: SaveData) => MapLocation[]) | MapLocation[];
+}[] = [
+  {
+    id: 1,
+    desc: 'Complete the Volatile Flintbeetles quest in Bone Bottom.',
+    has: saveData => getQuest('Rock Rollers', saveData)?.Data.IsCompleted ?? false,
+    markers: saveData => [
+      {
+        label: 'Promise the Volatile Flintbeetles wish.',
+        location: Locations.BoneBottom,
+      },
+      ...Flintbeetles.filter(beetle => !saveData.playerData[beetle.key]).flatMap(beetle =>
+        beetle.getMarker(saveData),
+      ),
+      {
+        label: 'Turn over the Flintgems to receive a Memory Locket.',
+        location: Locations.BoneBottom,
+      },
+    ],
+  },
+  {
+    id: 2,
+    desc: 'Purchased from Frey in Bellhart for 330 rosaries.',
+    has: saveData => saveData.playerData.PurchasedBelltownMemoryLocket,
+    markers: [
+      {
+        label: 'Purchased from Frey for 330 rosaries.',
+        location: Locations.Bellhart,
+      },
+    ],
+  },
+  {
+    id: 3,
+    desc: 'Found in a breakable cocoon in Bilewater.',
+    has: saveData =>
+      getScene('Shadow_27', 'Breakable Hang Sack Memory Locket', saveData)?.Value === false,
+    markers: [
+      {
+        label: 'Break the cocoon and collect the Memory Locket.',
+        location: { x: 4139, y: 1156 },
+      },
+    ],
+  },
+  {
+    id: 4,
+    desc: 'In a secret room in Bilewater, beyond a steam pipe.',
+    has: saveData => getScene('Shadow_20', 'Collectable_Item_Pickup', saveData)?.Value,
+    markers: [
+      {
+        label: 'Enter the secret room and collect the Memory Locket.',
+        location: { x: 3284, y: 1403 },
+      },
+    ],
+  },
+  {
+    id: 5,
+    desc: 'Below the right exit to the first room in the Blasted Steps.',
+    has: saveData => getScene('Coral_02', 'Collectable Item Pickup (1)', saveData)?.Value,
+    markers: [
+      {
+        label: 'Collect the Memory Locket.',
+        location: { x: 894, y: 1853 },
+      },
+    ],
+  },
+  {
+    id: 6,
+    desc: 'Held by a corpse at the bottom of a magma-filled area found behind a breakable wall in the lower Deep Docks.',
+    has: saveData => getScene('Dock_13', 'Collectable Item Pickup', saveData)?.Value,
+    markers: [
+      {
+        label: 'Collect the Memory Locket.',
+        location: { x: 2629, y: 3040 },
+      },
+    ],
+  },
+  {
+    id: 7,
+    desc: "Defeat the voided Skarrgard past Karmelita's room.",
+    has: saveData => getScene('Bone_East_25', 'Collectable Item Pickup', saveData)?.Value,
+    markers: [
+      {
+        label: 'Defeat the voided Skarrgard.',
+        location: { x: 4223, y: 2309 },
+      },
+    ],
+  },
+  {
+    id: 8,
+    desc: 'Found above the Grand Bellway in a secret area accessible from the Exhaust Organ side.',
+    has: saveData => getScene('Bellway_City', 'Collectable Item Pickup', saveData)?.Value,
+    markers: [
+      {
+        label: 'Found above the Grand Bellway.',
+        location: { x: 2913, y: 1153 },
+      },
+    ],
+  },
+  {
+    id: 9,
+    desc: 'Above the Mitemothers in Greymoor, to the left of and below the Bellway.',
+    has: saveData => getScene('Greymoor_16', 'Collectable Item Pickup', saveData)?.Value,
+    markers: [
+      {
+        label: 'Above the Mitemothers.',
+        location: { x: 2770, y: 2160 },
+      },
+    ],
+  },
+  {
+    id: 10,
+    desc: 'Inside the Halfway Home in Greymoor, on a ledge on the left side.',
+    has: saveData => getScene('Halfway_01', 'Collectable Item Pickup', saveData)?.Value,
+    markers: [
+      {
+        label: 'Inside the Halfway Home, on a ledge on the left side.',
+        location: Locations.HalfwayHome,
+      },
+    ],
+  },
+  {
+    id: 11,
+    desc: 'In a cage at the end of the spiky corridor before Chapel of the Beast.',
+    has: saveData => getScene('Ant_20', 'Collectable Item Pickup', saveData)?.Value,
+    markers: [
+      {
+        label: 'In a cage at the end of the spiky corridor before Chapel of the Beast.',
+        location: { x: 3252, y: 2458 },
+      },
+    ],
+  },
+  {
+    id: 12,
+    desc: 'Found in the Memorium.',
+    has: saveData => getScene('Arborium_05', 'Collectable Item Pickup', saveData)?.Value,
+    markers: [
+      {
+        label: 'Found in the Memorium.',
+        location: { x: 2785, y: 650 },
+      },
+    ],
+  },
+  {
+    id: 13,
+    desc: "Can be purchased from Mort in the Pilgrim's Rest in the Far Fields for 150 rosaries.",
+    has: saveData => saveData.playerData.PurchasedPilgrimsRestMemoryLocket,
+    markers: [
+      {
+        label: 'Can be purchased from Mort for 150 rosaries.',
+        location: Locations.Mort,
+      },
+    ],
+  },
+  {
+    id: 14,
+    desc: 'Held by a corpse in the top of the first area of the Sands of Karak.',
+    has: saveData => getScene('Coral_23', 'Collectable Item Pickup', saveData)?.Value,
+    markers: [
+      {
+        label: 'Pick up the Memory Locket from the corpse.',
+        location: { x: 430, y: 1495 },
+      },
+    ],
+  },
+  {
+    id: 15,
+    desc: 'Found after a mini-gauntlet in the top of the Marrow.',
+    has: saveData => getScene('Bone_18', 'Collectable Item Pickup', saveData)?.Value,
+    markers: [
+      {
+        label: 'Defeat the gauntlet here.',
+        location: { x: 1725, y: 2339 },
+      },
+    ],
+  },
+  {
+    id: 16,
+    desc: 'Found in a cave in the Slab.',
+    has: saveData => getScene('Slab_Cell_Quiet', 'Collectable Item Pickup', saveData)?.Value,
+    markers: [
+      {
+        label: 'Break a wall on the right while climbing up.',
+        location: { x: 1402, y: 1021 },
+      },
+    ],
+  },
+  {
+    id: 17,
+    desc: 'Found in the Underworks, in a hidden area above the rentable benches.',
+    has: saveData => getScene('Under_08', 'Collectable Item Pickup', saveData)?.Value,
+    markers: [
+      {
+        label: 'Climb up a hole in the ceiling and head left.',
+        location: { x: 2135, y: 1404 },
+      },
+    ],
+  },
+  {
+    id: 18,
+    desc: 'Found in the Whispering Vaults, in a secret room before the drop to Bilewater.',
+    has: saveData => getScene('Library_08', 'Collectable Item Pickup', saveData)?.Value,
+    markers: [
+      {
+        label: 'Climb up a hole in the ceiling and head left.',
+        location: { x: 3380, y: 944 },
+      },
+    ],
+  },
+  {
+    id: 19,
+    desc: 'Found in the Wormways, held by a corpse at the very end of the tunnels.',
+    has: saveData => getScene('Crawl_09', 'Collectable Item Pickup', saveData)?.Value,
+    markers: [
+      {
+        label: 'Pick up the Memory Locket from the corpse.',
+        location: { x: 493, y: 2544 },
+      },
+    ],
+  },
+  {
+    id: 20,
+    desc: 'Found by using Silk Soar from the top rung of Bellhart.',
+    has: saveData => getScene('Belltown', 'Collectable Item Pickup', saveData)?.Value,
+    markers: [
+      {
+        label: 'Found by using Silk Soar from the top rung of Bellhart.',
+        location: { x: 2158, y: 2111 },
+      },
+    ],
+  },
+];
