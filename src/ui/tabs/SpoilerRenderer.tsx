31c2a420397c707c4e4679d2d7ce836c85c9068d
PartMan <parth.mane@sprinklr.com>
2025-10-09 13:30:55 +0530

    feat: Improved spoiler renderers

--- src/ui/tabs/SpoilerRenderer.tsx
+++ src/ui/tabs/SpoilerRenderer.tsx
@@ -9,7 +9,7 @@ export function SpoilerRenderer({ content }: { content: string | null }): ReactE
       {content
         ? [...TextParser(content)].map(elem => {
             return elem.spoilerLevel <= currentSpoilerLevel ? (
-              <span>{elem.text}</span>
+              elem.text
             ) : (
               <SpoilerSpan text={elem.text} spoilerLevel={elem.spoilerLevel} />
             );
@@ -21,6 +21,7 @@ export function SpoilerRenderer({ content }: { content: string | null }): ReactE
 
 function SpoilerSpan({ text, spoilerLevel }: { text: string; spoilerLevel: number }): ReactElement {
   const [clicked, setClicked] = useState(false);
+
   const onKeyDown = (e: React.KeyboardEvent<HTMLSpanElement>) => {
     if (e.key === 'Enter' || e.key === ' ') {
       setClicked(true);
@@ -28,22 +29,30 @@ function SpoilerSpan({ text, spoilerLevel }: { text: string; spoilerLevel: numbe
       e.stopPropagation();
     }
   };
+
   return (
     <Tooltip content={`Spoilers for Act ${'I'.repeat(spoilerLevel)}`}>
-      {clicked ? (
-        <span tabIndex={0} onKeyDown={onKeyDown}>
-          {text}
-        </span>
-      ) : (
-        <span
-          tabIndex={0}
-          onKeyDown={onKeyDown}
-          className="bg-gray-700 text-gray-700 hover:bg-transparent hover:text-inherit transition-colors duration-300 cursor-pointer select-none"
-          onClick={() => setClicked(true)}
-        >
-          {text}
-        </span>
-      )}
+      <span className="relative min-w-30 inline-block group bg-stone-700 hover:bg-[#aaa6]">
+        {clicked ? (
+          <span tabIndex={0} onKeyDown={onKeyDown}>
+            {text}
+          </span>
+        ) : (
+          <>
+            <span
+              tabIndex={0}
+              onKeyDown={onKeyDown}
+              className="text-stone-700 group-hover:text-inherit transition-colors duration-300 cursor-pointer select-none relative"
+              onClick={() => setClicked(true)}
+            >
+              {text}
+            </span>
+            <span className="absolute w-full h-full top-0 left-0 flex items-center justify-center text-xs group-hover:invisible duration-0 text-stone-400">
+              (spoilers for Act {'I'.repeat(spoilerLevel)})
+            </span>
+          </>
+        )}
+      </span>
     </Tooltip>
   );
 }
