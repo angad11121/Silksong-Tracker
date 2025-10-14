0fc53f78a5196600601c691f9ec857110880d6cc
PartMan <parth.mane@sprinklr.com>
2025-10-14 18:59:22 +0530

    chore: Move tabs to individual page files

---
+++ src/ui/tabs/huntersJournal/page.tsx
@@ -0,0 +1,58 @@
+import type { ReactElement } from 'react';
+import { useSaveData } from '@/ui/hooks/useSaveData';
+import { SpoilerRenderer, useSpoilerLevel } from '@/ui/tabs/SpoilerRenderer';
+import { SectionRenderer } from '@/ui/tabs/SectionRenderer';
+import { getSections } from '@/ui/tabs/huntersJournal/sections';
+import { useSettings } from '@/ui/components/Settings';
+import { getSectionDisplayProps } from '@/ui/tabs/trueCompletion/page';
+import { Locations } from '@/info';
+import { LeafRenderer } from '@/ui/tabs/LeafRenderer';
+import HuntersJournal from '@/assets/journal.png';
+
+export function HuntersJournalDisplay(): ReactElement {
+  const data = useSaveData()!;
+  const showMissingFirst = useSettings('showMissingFirst');
+  const spoilerLevel = useSpoilerLevel();
+
+  return (
+    <div>
+      <p>
+        The Hunter's Journal is given by Nuu in the Halfway Home. There are a total of 236 entries
+        in the Hunter's Journal (237 in Steel Soul mode). For the Hunter's Memento, 230 of these
+        entries are required (231 in Steel Soul mode). 3 of the 6 non-required entries are missable.
+      </p>
+      <p className="text-center">
+        <LeafRenderer
+          id={null}
+          parents={[]}
+          icon={() => (
+            <img
+              src={HuntersJournal}
+              height={108}
+              width={108}
+              alt="Hunter's Journal"
+              className="inline"
+            />
+          )}
+          check={saveData => saveData.playerData.hasJournal}
+          hint=""
+          data={data}
+          markers={[
+            {
+              label: 'Talk to Nuu to start the Bugs of Pharloom quest.',
+              location: Locations.HalfwayHome,
+            },
+          ]}
+        />
+      </p>
+      <p>
+        <SpoilerRenderer content="The Steel-Soul exclusive enemy is the ||<3>Summoned Saviour||, while the missable entries are ||<2>Shakra||, ||<2>Garmond & Zaza||, and ||<3>Palestag||." />
+      </p>
+      <SectionRenderer
+        data={data}
+        sections={getSections(showMissingFirst, spoilerLevel, data.playerData.permadeathMode > 0)}
+        getSectionDisplayProps={(...props) => getSectionDisplayProps(...props, true)}
+      />
+    </div>
+  );
+}
---
+++ src/ui/tabs/percentage/page.tsx
@@ -0,0 +1,47 @@
+import { useMemo, type ReactElement } from 'react';
+import { SectionRenderer } from '@/ui/tabs/SectionRenderer';
+import { getSections } from '@/ui/tabs/percentage/sections';
+import { computePercentage } from '@/ui/tabs/utils';
+import { useSettings } from '@/ui/components/Settings';
+import { useSaveData } from '@/ui/hooks/useSaveData';
+import { useSpoilerLevel } from '@/ui/tabs/SpoilerRenderer';
+
+export function PercentageDisplay(): ReactElement {
+  const data = useSaveData()!;
+  const showMissingFirst = useSettings('showMissingFirst');
+  const spoilerLevel = useSpoilerLevel();
+  const sections = useMemo(
+    () => getSections(showMissingFirst, spoilerLevel),
+    [showMissingFirst, spoilerLevel],
+  );
+
+  return (
+    <SectionRenderer
+      sections={sections}
+      depth={0}
+      data={data}
+      getSectionDisplayProps={section => {
+        const currentPercentage = computePercentage(section.ctx.getPercentage, data);
+        const maxPercentage = section.ctx.maxPercentage;
+
+        return {
+          open: currentPercentage < maxPercentage,
+          After: () => (
+            <>
+              {' '}
+              (
+              {currentPercentage === maxPercentage ? (
+                <span className="text-green-500">{maxPercentage}%</span>
+              ) : (
+                <>
+                  <span className="text-red-500">{currentPercentage}%</span>/{maxPercentage}%
+                </>
+              )}
+              )
+            </>
+          ),
+        };
+      }}
+    />
+  );
+}
